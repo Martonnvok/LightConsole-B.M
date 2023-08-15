cba68356d3522b90051a8b657c3f2180a87d67c2
Martonnvok <115223813+Martonnvok@users.noreply.github.com>
2023-08-15 06:18:44 +0200

    LightConsole biztonsági mentés

---
+++ src/lightonconsole/LightOnConsole.java
@@ -0,0 +1,19 @@
+package lightonconsole;
+
+public class LightOnConsole {
+
+     public static void main(String[] args) {
+        foProgram();
+    }
+
+    private static void foProgram() {
+        kapcsolasokMegjelenese();
+    }
+
+    private static void kapcsolasokMegjelenese() {
+         JatekInditasa K = new JatekInditasa();
+    }
+    
+    
+
+}
